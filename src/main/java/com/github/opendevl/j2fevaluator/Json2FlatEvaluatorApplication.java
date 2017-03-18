2005c01fff68376841f0fffde7ae77caa494cce5
skapl <santanu.patel@suiit.ac.in>
2017-03-18 13:46:57 +0530

    1st commit j2flatEvaluator

---
+++ src/main/java/com/github/opendevl/j2fevaluator/Json2FlatEvaluatorApplication.java
@@ -0,0 +1,12 @@
+package com.github.opendevl.j2fevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Json2FlatEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Json2FlatEvaluatorApplication.class, args);
+	}
+}
